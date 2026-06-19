b78bd0a4f3f1427444ebe642dea9bd8029ff70d9
booherar <booherar@mail.uc.edu>
2026-06-18 22:44:03 -0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String trash = "";
+        double Celsius;
+        double Fahrenheit;
+    do {
+        System.out.print("Enter a temperature in Celsius: ");
+        if (scanner.hasNextDouble()) {
+            Celsius = scanner.nextDouble();
+            Fahrenheit = (Celsius * 9 / 5) + 32;
+            System.out.printf("%.2f degrees Celsius is equal to %.2f degrees Fahrenheit.%n", Celsius, Fahrenheit);
+        } else {
+            trash = scanner.nextLine(); // Consume the invalid input
+            System.out.println("Invalid input. Please enter a valid number for Celsius.");
+        }
+    } while (true);
+
+
+
+
+    }
+}
